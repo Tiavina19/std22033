fdc216b326af5e995efb141145d42b89c358f9ad
Tiavina19 <128223190+Tiavina19@users.noreply.github.com>
2025-02-10 16:04:17 +0300

    fix : edit code

--- src/main/java/com/example/demo/endpoint/rest/controller/health/HelloController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-public class PingController {
+public class HelloController {
 
   @GetMapping("/hello")
   public String hello() {
