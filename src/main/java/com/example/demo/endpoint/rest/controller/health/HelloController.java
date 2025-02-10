12d0c9862ab33d328ce7d9f6520bd555e918f211
Tiavina19 <128223190+Tiavina19@users.noreply.github.com>
2025-02-10 15:40:19 +0300

    feat: add hello controller

--- src/main/java/com/example/demo/endpoint/rest/controller/health/HelloController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/HelloController.java
@@ -7,18 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@PojaGenerated
+
 @RestController
 @AllArgsConstructor
 public class PingController {
 
-  public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
-  public static final ResponseEntity<String> KO =
-      new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
-
-  @GetMapping("/ping")
-  public String ping() {
-    return "pong";
+  @GetMapping("/hello")
+  public String hello() {
+    return "Hello std22033";
   }
 }
-j
